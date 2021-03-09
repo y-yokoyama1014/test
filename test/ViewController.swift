5e7167a85db3c1e08630f0a2c2dfdbd430863453
y.yokoyama <tsuna1014uu@gmail.com>
2021-03-09 14:25:08 +0900

    commitテスト

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("テスト")
         // Do any additional setup after loading the view.
     }
 
